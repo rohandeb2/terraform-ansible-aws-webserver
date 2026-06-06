354eca4c623e14251348da382cbbc864d216f00c
Ruhon Deb <145097266+rohandeb2@users.noreply.github.com>
2026-06-07 01:22:33 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "devops-training-tfstate-959589242185"  # from bootstrap output
+    bucket         = "devops-training-tfstate-959589242185"  
     key            = "devops-training/terraform.tfstate"
     region         = "ap-south-1"
     encrypt        = true
-    dynamodb_table = "terraform-state-lock"                  # from bootstrap output
+    dynamodb_table = "terraform-state-lock"                 
   }
 }
